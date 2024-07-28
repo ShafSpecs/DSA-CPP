849e8e53305d5509f35cbe867a174ea240c612e7
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-07-28 04:48:07 +0100

    Added a very basic test for the binary tree

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -3,8 +3,7 @@
 template<typename T>
 struct TreeNode {
     T value;
-    TreeNode* right;
-    TreeNode* left;
+    TreeNode *right, *left;
 
     explicit TreeNode(T val):
         value(val),
@@ -13,3 +12,10 @@ struct TreeNode {
 };
 
 // This is a basic BT, right?!
+
+// void test() {
+//     auto* root = new TreeNode<int>(5);
+//     root->left = new TreeNode<int>(3);
+//     root->right = new TreeNode<int>(9);
+//     root->left->left = new TreeNode<int>(0);
+// } // basic test - alloc leakage for some reason too 🤔
